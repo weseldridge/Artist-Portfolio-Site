c9f948272240586728bbfc0ef9df472a72f2dfca
Wes Eldridge <weseldridge@gmail.com>
2014-03-14 09:51:37 -0500

    update to routes... move add and update to admin controller

--- src/app/routes.php
+++ src/app/routes.php
@@ -25,17 +25,49 @@ Route::post('register', array('uses' => 'UserController@doRegister'));
 
 /*
 * ----------------------------------------------------------------------------
+*							Admin Routes
+* ----------------------------------------------------------------------------
+*/
+// Admin Access Only
+Route::group(array('before' => 'admin_level_0'), function()
+{
+	Route::get('admin', array('uses' => 'AdminController@showAdmin'));
+	Route::get('admin/user', array('uses' => 'AdminController@showAdminUser'));
+	Route::post('admin/user', array('uses' => 'AdminController@doAdminUser'));
+	/*
+	* ---------------------------------------------------------------------
+	*							Item Admin Routes
+	* ---------------------------------------------------------------------
+	*/
+	Route::get('item/edit/{id}', array('uses' => 'AdminController@showEditItem'));
+	Route::post('item/edit/{id}', array('uses' => 'AdminController@doEditItem'));
+	Route::get('item/add', array('uses' => 'AdminController@showAddItem'));
+	Route::post('item/add', array('uses' => 'AdminController@doAddItem'));
+	/*
+	* ---------------------------------------------------------------------
+	*							Gallery Admin Routes
+	* ---------------------------------------------------------------------
+	*/
+	Route::get('gallery/add', array('uses' => 'AdminController@showAddGallery'));
+	Route::post('gallery/add', array('uses' => 'AdminController@doAddGallery'));
+	Route::get('gallery/edit/{id}', array('uses' => 'AdminController@showEditThisGallery'))->where('id', '[0-9]+');
+	Route::post('gallery/edit/{id}', array('uses' => 'AdminController@doEditThisGallery'))->where('id', '[0-9]+');
+	/*
+	* ---------------------------------------------------------------------
+	*							Group Admin Routes
+	* ---------------------------------------------------------------------
+	*/
+	Route::get('group/add', array('uses' => 'AdminController@showAddGroup'));
+	Route::post('group/add', array('uses' => 'AdminController@doAddGroup'));
+	Route::get('group/edit/{id}', array('uses' => 'AdminController@showEditThisGroup'))->where('id', '[0-9]+');
+	Route::post('group/edit/{id}', array('uses' => 'AdminController@doEditThisGroup'))->where('id', '[0-9]+');
+});
+
+/*
+* ----------------------------------------------------------------------------
 *							Gallery Routes
 * ----------------------------------------------------------------------------
 */
-Route::group(array('before' => 'admin_level_0'), function()
-{
-// Admin Access Only
-	Route::get('gallery/add', array('uses' => 'GalleryController@showAddGallery'));
-	Route::post('gallery/add', array('uses' => 'GalleryController@doAddGallery'));
-	Route::get('gallery/edit/{id}', array('uses' => 'GalleryController@showEditThisGallery'))->where('id', '[0-9]+');
-	Route::post('gallery/edit/{id}', array('uses' => 'GalleryController@doEditThisGallery'))->where('id', '[0-9]+');
-});
 // Public Access
 Route::get('gallery', array('uses' => 'GalleryController@showGallery'));
 Route::get('gallery/{id}', array('uses' => 'GalleryController@showThisGalleryById'))->where('id', '[0-9]+');
@@ -46,15 +78,6 @@ Route::get('gallery/{name}', array('uses' => 'GalleryController@showThisGalleryB
 *							Item Routes
 * ----------------------------------------------------------------------------
 */
-
-// Admin Access Only
-Route::group(array('before' => 'admin_level_0'), function()
-{
-	Route::get('item/edit/{id}', array('uses' => 'ItemController@showEditThisItem'));
-	Route::post('item/edit/{id}', array('uses' => 'ItemController@doEditThisItem'));
-	Route::get('item/add', array('uses' => 'ItemController@showAddItem'));
-	Route::post('item/add', array('uses' => 'ItemController@doAddItem'));
-});
 // Public Access
 Route::get('item', array('uses' => 'ItemController@showItem'));
 Route::get('item/{id}', array('uses' => 'ItemController@showThisItem'));
@@ -64,39 +87,18 @@ Route::get('item/{id}', array('uses' => 'ItemController@showThisItem'));
 *							Group Routes
 * ----------------------------------------------------------------------------
 */
-
-// Admin Access Only
-Route::group(array('before' => 'admin_level_0'), function()
-{
-	Route::get('group/add', array('uses' => 'GroupController@showAddGroup'));
-	Route::post('group/add', array('uses' => 'GroupController@doAddGroup'));
-	Route::get('group/edit/{id}', array('uses' => 'GroupController@showEditThisGroup'))->where('id', '[0-9]+');
-	Route::post('group/edit/{id}', array('uses' => 'GroupController@doEditThisGroup'))->where('id', '[0-9]+');
-});
 // Public Access
 Route::get('group', array('uses' => 'GroupController@showGroup'));
 Route::get('group/{id}', array('uses' => 'GroupController@showThisGroupById'))->where('id', '[0-9]+');
 Route::get('group/{name}', array('uses' => 'GroupController@showThisGroupByName'))->where('name', '[A-Za-z0-9]+');
 
-/*
-* ----------------------------------------------------------------------------
-*							Admin Routes
-* ----------------------------------------------------------------------------
-*/
-// Admin Access Only
-Route::group(array('before' => 'admin_level_0'), function()
-{
-	Route::get('admin', array('uses' => 'AdminController@showAdmin'));
-	Route::get('admin/user', array('uses' => 'AdminController@showAdminUser'));
-	Route::post('admin/user', array('uses' => 'AdminController@doAdminUser'));
-});
+
 /*
 * ----------------------------------------------------------------------------
 *							Other Routes
 * ----------------------------------------------------------------------------
 */
 Route::get('/', array('uses' => 'HomeController@showHome'));
-
 
 
 /*
