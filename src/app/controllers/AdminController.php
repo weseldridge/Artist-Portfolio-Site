e394f1caa80d66c10408968fd8a83227c466cd03
Wes Eldridge <weseldridge@gmail.com>
2014-03-14 10:05:57 -0500

    moved methods from add, edit, and support methods from group to admin

--- src/app/controllers/AdminController.php
+++ src/app/controllers/AdminController.php
@@ -4,7 +4,9 @@ class AdminController extends BaseController {
 
 	/*
 	* ----------------------------------------------------------------------------
+	* ----------------------------------------------------------------------------
 	*							GET METHODS
+	* ----------------------------------------------------------------------------
 	* ----------------------------------------------------------------------------
 	*/
 
@@ -20,6 +22,31 @@ class AdminController extends BaseController {
 	*/
 	public function showAdminUser(){
 		$this->layout->content = View::make('admins.user');
+	}
+
+	/*
+	* ----------------------------------------------------------------------------
+	*							GROUP METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
+	/**
+	*
+	*/
+	public function showEditThisGroup($id) {
+		$group = Group::find($id);
+		$items = Item::all();
+		$this->layout->content = View::make('groups.editThis')->with('id', $id)
+															  ->with('group', $group)
+															  ->with('items', $items);
+	}
+
+	/**
+	*
+	*/
+	public function showAddGroup() {
+		$items = Item::all();
+		$this->layout->content = View::make('groups.addGroup')->with('items', $items);
 	}
 
 	/*
@@ -47,9 +74,72 @@ class AdminController extends BaseController {
 
 	/*
 	* ----------------------------------------------------------------------------
+	* ----------------------------------------------------------------------------
 	*							POST METHODS
 	* ----------------------------------------------------------------------------
+	* ----------------------------------------------------------------------------
 	*/
+
+	/*
+	* ----------------------------------------------------------------------------
+	*							GROUP METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
+	/**
+	*
+	*/
+	public function doEditThisGroup($id) {
+
+		// validate the info, create rules for the inputs
+		$rules = array(
+				'name'    		=> 'required|min:3|max:49|unique:groups',
+				'description' 	=> 'min:3|max:250',
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('group/edit/' . $id)
+				->withErrors($validator);
+		} else {
+			$group = Group::find($id);
+			$group->name = Input::get('name');
+			$group->description = Input::get('description');
+			$group->save();
+
+			return Redirect::to('/')->with('message', 'Group successfully updated.');
+		}
+
+	}
+
+	/**
+	*
+	*/
+	public function doAddGroup() {
+
+		$rules = array(
+				'name' 			=> 'required|min:3|max:49|unique:groups',
+				'description'	=> 'min:3|max:250',
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('group/add')->with('message', 'The following errors occurred: ')->withErrors($validator)->withInput();
+		} else {
+			$group = new Group;
+		    $group->name = Input::get('name');
+		    $group->description = Input::get('description');
+		    $group->save();
+
+		    return Redirect::to('/')->with('message', 'Group successfully added.');
+		}
+	}
 
 	/*
 	* ----------------------------------------------------------------------------
--- src/app/controllers/GroupController.php
+++ src/app/controllers/GroupController.php
@@ -41,85 +41,13 @@ public function showThisGroupByName($input) {
 }
 
 
-/**
-*
-*/
-public function showEditThisGroup($id) {
-	$group = Group::find($id);
-	$items = Item::all();
-	$this->layout->content = View::make('groups.editThis')->with('id', $id)
-														  ->with('group', $group)
-														  ->with('items', $items);
-}
-
-/**
-*
-*/
-public function showAddGroup() {
-	$items = Item::all();
-	$this->layout->content = View::make('groups.addGroup')->with('items', $items);
-}
-
 /*
 * ----------------------------------------------------------------------------
 *							POST METHODS
 * ----------------------------------------------------------------------------
 */
 
-/**
-*
-*/
-public function doEditThisGroup($id) {
 
-	// validate the info, create rules for the inputs
-	$rules = array(
-			'name'    		=> 'required|min:3|max:49|unique:groups',
-			'description' 	=> 'min:3|max:250',
-		);
-
-	// run the validation rules on the inputs from the form
-	$validator = Validator::make(Input::all(), $rules);
-
-	// if the validator fails, redirect back to the form
-	if ($validator->fails()) {
-		return Redirect::to('group/edit/' . $id)
-			->withErrors($validator);
-	} else {
-		$group = Group::find($id);
-		$group->name = Input::get('name');
-		$group->description = Input::get('description');
-		$group->save();
-
-		return Redirect::to('/')->with('message', 'Group successfully updated.');
-	}
-
-}
-
-/**
-*
-*/
-public function doAddGroup() {
-
-	$rules = array(
-			'name' 			=> 'required|min:3|max:49|unique:groups',
-			'description'	=> 'min:3|max:250',
-		);
-
-	// run the validation rules on the inputs from the form
-	$validator = Validator::make(Input::all(), $rules);
-
-	// if the validator fails, redirect back to the form
-	if ($validator->fails()) {
-		return Redirect::to('group/add')->with('message', 'The following errors occurred: ')->withErrors($validator)->withInput();
-	} else {
-		$group = new Group;
-	    $group->name = Input::get('name');
-	    $group->description = Input::get('description');
-	    $group->save();
-
-	    return Redirect::to('/')->with('message', 'Group successfully added.');
-	}
-}
 
 
 /*
