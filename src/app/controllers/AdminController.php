4c7cf6c84ee86e4e206708593258576ff981d4d8
Wes Eldridge <weseldridge@gmail.com>
2014-03-14 10:00:22 -0500

    moved methods from add, edit, and support methods from gallyer to admin

--- src/app/controllers/AdminController.php
+++ src/app/controllers/AdminController.php
@@ -2,6 +2,12 @@
 
 class AdminController extends BaseController {
 
+	/*
+	* ----------------------------------------------------------------------------
+	*							GET METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
 	/**
 	*
 	*/
@@ -16,11 +22,104 @@ class AdminController extends BaseController {
 		$this->layout->content = View::make('admins.user');
 	}
 
+	/*
+	* ----------------------------------------------------------------------------
+	*							Gallery METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
+	/**
+	*
+	*/
+	public function showAddGallery() {
+		$this->layout->content = View::make('galleries.addGallery');
+	}
+
+	/**
+	*
+	*/
+	public function showEditThisGallery($id) {
+		$gallery = Gallery::find($id);
+		$this->layout->content = View::make('galleries.editThis')->with('id', $id)
+																 ->with('gallery', $gallery);
+	}
+
+
+	/*
+	* ----------------------------------------------------------------------------
+	*							POST METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
+	/*
+	* ----------------------------------------------------------------------------
+	*							Gallery METHODS
+	* ----------------------------------------------------------------------------
+	*/
+
+	/**
+	*
+	*/
+	public function doEditThisGallery($id) {
+		// validate the info, create rules for the inputs
+		$rules = array(
+				'name'    		=> 'required|min:3|max:49|unique:groups',
+				'description' 	=> 'min:3|max:250',
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+		
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('gallery/edit/' . $id)
+				->withErrors($validator);
+		} else {
+			$gallery = Gallery::find($id);
+			$gallery->name = Input::get('name');
+			$gallery->description = Input::get('description');
+			$gallery->save();
+
+			return Redirect::to('/')->with('message', 'Gallery successfully updated.');
+		}
+	}
+
+	/**
+	*
+	*/
+	public function doAddGallery() {
+		$rules = array(
+				'name' 			=> 'required|min:3|max:49|unique:groups',
+				'description'	=> 'min:3|max:250',
+			);
+
+		// run the validation rules on the inputs from the form
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('gallery/add')->with('message', 'The following errors occurred: ')->withErrors($validator)->withInput();
+		} else {
+			$gallery = new Gallery;
+		    $gallery->name = Input::get('name');
+		    $gallery->description = Input::get('description');
+		    $gallery->save();
+
+		    return Redirect::to('/')->with('message', 'Gallery successfully added.');
+		}
+	}
+
 	/**
 	*
 	*/
 	public function doAdminUser(){
 		
 	}
+
+	/*
+	* ----------------------------------------------------------------------------
+	*							HELPER METHODS
+	* ----------------------------------------------------------------------------
+	*/
 
 }
--- src/app/controllers/GalleryController.php
+++ src/app/controllers/GalleryController.php
@@ -39,21 +39,6 @@ public function showThisGalleryByName($input) {
 															 	->with('gallery', $gallery);
 }
 
-/**
-*
-*/
-public function showAddGallery() {
-	$this->layout->content = View::make('galleries.addGallery');
-}
-
-/**
-*
-*/
-public function showEditThisGallery($id) {
-	$gallery = Gallery::find($id);
-	$this->layout->content = View::make('galleries.editThis')->with('id', $id)
-															 ->with('gallery', $gallery);
-}
 
 /*
 * ----------------------------------------------------------------------------
@@ -61,57 +46,6 @@ public function showEditThisGallery($id) {
 * ----------------------------------------------------------------------------
 */
 
-/**
-*
-*/
-public function doEditThisGallery($id) {
-	// validate the info, create rules for the inputs
-	$rules = array(
-			'name'    		=> 'required|min:3|max:49|unique:groups',
-			'description' 	=> 'min:3|max:250',
-		);
-
-	// run the validation rules on the inputs from the form
-	$validator = Validator::make(Input::all(), $rules);
-	
-	// if the validator fails, redirect back to the form
-	if ($validator->fails()) {
-		return Redirect::to('gallery/edit/' . $id)
-			->withErrors($validator);
-	} else {
-		$gallery = Gallery::find($id);
-		$gallery->name = Input::get('name');
-		$gallery->description = Input::get('description');
-		$gallery->save();
-
-		return Redirect::to('/')->with('message', 'Gallery successfully updated.');
-	}
-}
-
-/**
-*
-*/
-public function doAddGallery() {
-	$rules = array(
-			'name' 			=> 'required|min:3|max:49|unique:groups',
-			'description'	=> 'min:3|max:250',
-		);
-
-	// run the validation rules on the inputs from the form
-	$validator = Validator::make(Input::all(), $rules);
-
-	// if the validator fails, redirect back to the form
-	if ($validator->fails()) {
-		return Redirect::to('gallery/add')->with('message', 'The following errors occurred: ')->withErrors($validator)->withInput();
-	} else {
-		$gallery = new Gallery;
-	    $gallery->name = Input::get('name');
-	    $gallery->description = Input::get('description');
-	    $gallery->save();
-
-	    return Redirect::to('/')->with('message', 'Gallery successfully added.');
-	}
-}
 
 
 /*
